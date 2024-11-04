c14bf447b7f1aefffccb266bbd38a50eb797183e
Amit Mahadik <amit.mahadik@stryker.com>
2024-11-03 19:55:38 -0800

    Initial commit

---
+++ MemorizeApp/MemorizeApp/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Mahadik, Amit on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
