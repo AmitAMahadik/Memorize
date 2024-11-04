01dd4b32f77e14c4787364f325f0a1c8829cd962
Amit Mahadik <amit.mahadik@stryker.com>
2024-11-03 22:55:56 -0800

    Wrapped up lecture 2

--- MemorizeApp/MemorizeApp/ContentView.swift
+++ MemorizeApp/MemorizeApp/ContentView.swift
@@ -8,21 +8,59 @@
 import SwiftUI
 
 struct ContentView: View { // ContentView "behaves like a" View
-    let emojis = ["👻", "🎃", "😈", "🕷️"] // "Inferred Array of String" emojis: Array<String>
+    let emojis = ["👻", "🎃", "😈", "🕷️", "🤖", "👽", "👾"] // "Inferred Array of String" emojis: Array<String>
+    
+    @State var cardCount: Int = 4
     
     var body: some View { // Computed Property - Lego analogy
-        HStack {
-            ForEach(emojis.indices, id: \.self) { emoji in
-                CardView(cardContent: emojis[emoji])
+        VStack {
+            ScrollView {
+                cards
             }
-/*            CardView(cardContent: "👻", isFaceUp: true)
-            CardView(cardContent: "🎃")
-            CardView(cardContent: "😈")
-            CardView(cardContent: "🕷️", isFaceUp: true) */
+            cardCountAdjuster
         }
-        .foregroundColor(.orange)
         .padding()
+        }
+    
+    var cards: some View {
+        //  HStack { // Implicit return doesn't require return since it's only one line of code
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { emoji in
+                CardView(cardContent: emojis[emoji])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
     }
+        
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardCountAdjuster: some View {
+   //     Text("\(cardCount)")
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
 }
 
 struct CardView: View {
@@ -35,13 +73,14 @@ struct CardView: View {
         ZStack(alignment: .center, content: { // Tuple view
             let cardBase =  RoundedRectangle(cornerRadius: 12) //Type Inference
             //let cardBase: RoundedRectangle =  RoundedRectangle(cornerRadius: 12) // Lego blocks cornerRadius: Named parameter //let is constant
-            if isFaceUp {
+            Group {
                 cardBase.fill(.white)
                 cardBase.strokeBorder(lineWidth: 10)
                 Text(cardContent).font(.largeTitle) // Text struct
-            } else {
-                cardBase.fill()
             }
+            .opacity(isFaceUp ? 1 : 0)
+            cardBase.fill().opacity(isFaceUp ? 0 : 1)
+            
         })
         .onTapGesture {
             // print("Tapped")
